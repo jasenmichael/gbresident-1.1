157aa554ec61bb22eec6754d3aa7c8c0ce21be13
jasenmichael <jasen@ready-builder.com>
2019-05-29 10:59:44 -0500

    deploy-to-heroku

--- inc/db-connection.php
+++ inc/db-connection.php
@@ -3,6 +3,8 @@ set_time_limit(0);
 ini_set("memory_limit","-1");
 
 $servername = "us-cdbr-iron-east-02.cleardb.net";
+// $servername = getenv('DB_HOST');
+
 $username = "b38cbdfaae4f40";
 $password = "d94ce7a6";
 $db = 'heroku_4fafaa7172f3435';

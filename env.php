a1622d6e9440913b0a5928fee8b23545df82603b
jasenmichael <jasen@ready-builder.com>
2019-05-29 11:58:01 -0500

    deploy-to-heroku

---
+++ env.php
@@ -0,0 +1,8 @@
+<?php
+
+$DB = [
+    "DB_HOST" => "us-cdbr-iron-east-02.cleardb.net",
+    "DB_USER" => "b38cbdfaae4f40",
+    "DB_PASSWORD" => "d94ce7a6",
+    "DB_NAME" => "heroku_4fafaa7172f3435",
+];
--- inc/db-connection.php
+++ inc/db-connection.php
@@ -1,11 +1,21 @@
 <?php
 set_time_limit(0);
-ini_set("memory_limit","-1");
+ini_set("memory_limit", "-1");
 
-$servername = "us-cdbr-iron-east-02.cleardb.net";
-// $servername = getenv('DB_HOST');
+if (file_exists("../../env.php")) {
+    $env = "production";
+    include("../../env.php");
+}  else{
+    $env = "development";
+}
 
-$username = "b38cbdfaae4f40";
+// $servername = "us-cdbr-iron-east-02.cleardb.net";
+// $servername = DB['DB_HOST'] || getenv('DB_HOST');
+$servername = ($env = "development" ? $DB['DB_HOST'] : getenv('DB_HOST'));
+
+// $username = "b38cbdfaae4f40";
+$username = ($env = "development" ? $DB['DB_USER'] : getenv('DB_USER'));
+
 $password = "d94ce7a6";
 $db = 'heroku_4fafaa7172f3435';
 // Create connection
@@ -13,6 +23,7 @@ $conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
+    echo $servername;
     die("Connection failed: " . $conn->connect_error);
 } 
 // echo "Connected successfully";

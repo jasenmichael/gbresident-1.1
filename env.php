610e83bff1eb3647e49dde34b56660ef6d4cc3a6
jasenmichael <jasen@ready-builder.com>
2019-05-29 12:15:47 -0500

    deploy-to-heroku

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$DB = [
-    "DB_HOST" => "us-cdbr-iron-east-02.cleardb.net",
-    "DB_USER" => "b38cbdfaae4f40",
-    "DB_PASSWORD" => "d94ce7a6",
-    "DB_NAME" => "heroku_4fafaa7172f3435",
-];

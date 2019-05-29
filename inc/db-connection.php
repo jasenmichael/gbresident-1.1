4e6e6da4c6e45ed12b16ec53c0f5e9bf4c2e86f7
jasenmichael <jasen@ready-builder.com>
2019-05-29 12:07:30 -0500

    deploy-to-heroku

--- inc/db-connection.php
+++ inc/db-connection.php
@@ -9,11 +9,8 @@ if (file_exists("../../env.php")) {
     $env = "development";
 }
 
-// $servername = "us-cdbr-iron-east-02.cleardb.net";
-// $servername = DB['DB_HOST'] || getenv('DB_HOST');
 $servername = ($env = "development" ? $DB['DB_HOST'] : getenv('DB_HOST'));
 
-// $username = "b38cbdfaae4f40";
 $username = ($env = "development" ? $DB['DB_USER'] : getenv('DB_USER'));
 
 $password = "d94ce7a6";
@@ -23,7 +20,6 @@ $conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
-    echo $servername;
     die("Connection failed: " . $conn->connect_error);
 } 
 // echo "Connected successfully";

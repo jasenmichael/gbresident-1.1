3433eea67d25fa818fbf56a6faffb5e015f12c15
jasenmichael <jasen@ready-builder.com>
2019-05-28 22:36:53 -0500

    deploy-to-heroku

--- inc/password_protect.php
+++ inc/password_protect.php
@@ -118,9 +118,9 @@ function showLoginPasswordProtect($error_msg) {
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="/var/www/public_html/assets/js/jquery.min.js"></script>
+    <script src="./assets/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/var/www/public_html/assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -1,75 +1,89 @@
- <?php require(__DIR__ . '/../inc/password_protect.php'); ?> 
+ <?php
+  $PUBLIC = getenv('PUBLIC');
+  require(__DIR__ . '/../inc/password_protect.php');
+  ?>
 
-<!doctype html>
-<html lang="en">
-<head>
+ <!doctype html>
+ <html lang="en">
 
-
-<!-- header here      -->
- <?php require(__DIR__ . '/../inc/header.php'); ?> 
-</head>
-<body>
-
-<!-- navbar here      -->
- <?php require(__DIR__ . '/../inc/navbar.php'); ?> 
-
-<!-- body here      -->
-
-  
-    <div class="jumbotron">
-      <div class="container">
-
-          <div id="outer" style="width:100%"><div id="inner"><!-- center content  --> 
-             <h1><center>Greenbriar Resident<br /><small>Information, Forms and Documents.</small></center></h1>
-          </div></div><!-- end center content  --> 
-
-      </div>
-    </div>              
+ <head>
 
 
+   <!-- header here      -->
+   <?php require(__DIR__ . '/../inc/header.php'); ?>
+ </head>
 
-  <div id="main">
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4" "col-xs-2">
-          <h2>Forms</h2>
-          <p>More online forms, and printable forms to come, but for now we can now use this form for submitting hours. </p>
-          <p><a class="btn btn-default" href="forms/work-form.php" role="button">Work Form &raquo;</a></p>
-        </div>
+ <body>
 
+   <!-- navbar here      -->
+   <?php require(__DIR__ . '/../inc/navbar.php'); ?>
 
-        <div class="col-md-4" "col-xs-2">
-          <h2>Submitted Hours Archive</h2>
-          <p>The work form outputs a monthly pdf for easy book-keeping.</p>
-          <p><a class="btn btn-default" href="files/?dir=archive/work-credit-submissions" role="button">Check it out &raquo;</a></p>
-
-       </div>  
-
-        <div class="col-md-4" "col-xs-2">
-          <h2>Documents & Files</h2>
-          <p>Important Greenbriar documents and archives. Take a look around, stay tuned, more files to come.</p>
-          <p><a class="btn btn-default" href="files" role="button">Files and such &raquo;</a></p>
-       </div>      
-      </div>
-      <hr>
-    </div> <!-- /container -->     
-  </div>
+   <!-- body here      -->
+   <?php 
+   echo "<h2>yo: ". $PUBLIC ."</h2>";
+   ?>
 
 
-<!-- body end here      -->
+   <div class="jumbotron">
+     <div class="container">
 
-<!-- footer here      -->
-      <footer class="text-center">
- <?php require(__DIR__ . '/../inc/footer.php'); ?> 
-      </footer>
+       <div id="outer" style="width:100%">
+         <div id="inner">
+           <!-- center content  -->
+           <h1>
+             <center>Greenbriar Resident<br /><small>Information, Forms and Documents.</small></center>
+           </h1>
+         </div>
+       </div><!-- end center content  -->
+
+     </div>
+   </div>
 
 
 
-<script src="/assets/js/vendor/jquery-1.11.2.min.js"></script>
+   <div id="main">
+     <div class="container">
+       <!-- Example row of columns -->
+       <div class="row">
+         <div class="col-md-4" "col-xs-2">
+           <h2>Forms</h2>
+           <p>More online forms, and printable forms to come, but for now we can now use this form for submitting hours. </p>
+           <p><a class="btn btn-default" href="forms/work-form.php" role="button">Work Form &raquo;</a></p>
+         </div>
 
-<script src="/assets/js/vendor/bootstrap.min.js"></script>
 
-</body>
-</html>
-<!-- IE needs 512+ bytes: http://blogs.msdn.com/b/ieinternals/archive/2010/08/19/http-error-pages-in-internet-explorer.aspx -->
+         <div class="col-md-4" "col-xs-2">
+           <h2>Submitted Hours Archive</h2>
+           <p>The work form outputs a monthly pdf for easy book-keeping.</p>
+           <p><a class="btn btn-default" href="files/?dir=archive/work-credit-submissions" role="button">Check it out &raquo;</a></p>
+
+         </div>
+
+         <div class="col-md-4" "col-xs-2">
+           <h2>Documents & Files</h2>
+           <p>Important Greenbriar documents and archives. Take a look around, stay tuned, more files to come.</p>
+           <p><a class="btn btn-default" href="files" role="button">Files and such &raquo;</a></p>
+         </div>
+       </div>
+       <hr>
+     </div> <!-- /container -->
+   </div>
+
+
+   <!-- body end here      -->
+
+   <!-- footer here      -->
+   <footer class="text-center">
+     <?php require(__DIR__ . '/../inc/footer.php'); ?>
+   </footer>
+
+
+
+   <script src="/assets/js/vendor/jquery-1.11.2.min.js"></script>
+
+   <script src="/assets/js/vendor/bootstrap.min.js"></script>
+
+ </body>
+
+ </html>
+ <!-- IE needs 512+ bytes: http://blogs.msdn.com/b/ieinternals/archive/2010/08/19/http-error-pages-in-internet-explorer.aspx -->
